7b03102d0121ecf064401dec9858fb54fbdb96d2
Ana <ulsa@Pol.local>
2019-09-13 14:36:52 -0700

    Nuevo

--- TableViews/CeldaAlumnoController.swift
+++ TableViews/CeldaAlumnoController.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+import UIKit
+
+class CeldaAlumnoController : UITableViewCell{
+    
+    @IBOutlet weak var lblNombre: UILabel!
+}
---
+++ TableViews/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Alumno on 9/13/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let alumnos = ["Gabriel", "Ana Lourdes", "Joel", "Jose Trejo", "Araceli", "Karen"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    //Numero de filas
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumnos.count
+    }
+    //Contenido de cada celda
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno")
+        
+        return celda!
+    }
+
+
+}
+
